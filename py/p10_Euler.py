8f206eeee5ee4d58994a5e5e9738ebe2857afd06
sina <sinasohrab84@gmail.com>
2023-07-05 15:44:33 +0330

    developing

--- py/p10_Euler.py
+++ py/p10_Euler.py
@@ -1,8 +1,18 @@
-number=int(input("your number: "))
-for i in range(2, number):
-    if number % i == 0:
-        aval=False
+
+def is_prim(n):
+    aval = True
+    for i in range(2, n/2):
+        if n % i == 0:
+            aval = False
+    return aval
+
+firs=0
+secend=0
+
+for i in range(2,2000000):
+    if is_prim(i):
+        firs=i
+        sum=firs+secend
+        secend=sum
         
-        
-if aval:
-    print(i)
+print(sum)
