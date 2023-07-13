0d94713a35c745ba5524a52962e70cac314b8682
sina <sinasohrab84@gmail.com>
2023-07-13 23:23:52 +0330

    Developing p2

--- Euler_py/p7_Euler.py
+++ Euler_py/p7_Euler.py
@@ -7,9 +7,17 @@ def is_prime(n):
             return False
     return True
 
-sum=0
+
+sum = 0
 for i in range(2, number):
     if is_prime(i):
-        sum+=1
+        ii=i
+        sum += 1
 
-print(sum)
+taghsim = sum / 2
+int(taghsim)
+
+for i in range(1, number):
+    if is_prime(i):    
+        if i <= taghsim:
+            print("taghsim:", ii, sum,)
