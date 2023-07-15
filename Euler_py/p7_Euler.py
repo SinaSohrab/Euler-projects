b4ceb6a2ae6cab3609d46b743095ff28b6904553
sina <sinasohrab84@gmail.com>
2023-07-15 14:04:03 +0330

    Completion of the p7_Euler project

--- Euler_py/p7_Euler.py
+++ Euler_py/p7_Euler.py
@@ -1,23 +1,15 @@
-number = int(input("your number: :"))
+def find_nth_prime(n):
+    primes = [2]
+    num = 3
+    while len(primes) < n:
+        is_prime = True
+        for prime in primes:
+            if num % prime == 0:
+                is_prime = False
+                break
+        if is_prime:
+            primes.append(num)
+        num += 2
+    return primes[-1]
 
-
-def is_prime(n):
-    for i in range(2, int(n)):
-        if n % i == 0 and n % 1 == 0:
-            return False
-    return True
-
-
-sum = 0
-for i in range(2, number):
-    if is_prime(i):
-        ii=i
-        sum += 1
-
-taghsim = sum / 2
-int(taghsim)
-
-for i in range(1, number):
-    if is_prime(i):    
-        if i <= taghsim:
-            print("taghsim:", ii, sum,)
+print(find_nth_prime(10001))
