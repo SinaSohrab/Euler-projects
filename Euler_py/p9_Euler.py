452abc9e2eddc2d51f72793aad07b932898180c5
sina <sinasohrab84@gmail.com>
2023-07-18 10:55:13 +0330

    developing

---
+++ Euler_py/p9_Euler.py
@@ -0,0 +1,8 @@
+for number in range(0,1000):
+    this_number=str(number).zfill(2)
+
+    number_digits={}
+    number_digits["a"] = int(this_number[0])
+    number_digits["b"] = int(this_number[1])
+    number_digits["c"] = int(this_number[2])
+    
