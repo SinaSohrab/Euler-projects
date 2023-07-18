a042436f9dd05919ea9f3e4c6d57747df834aa3d
sina <sinasohrab84@gmail.com>
2023-07-18 11:33:13 +0330

    felan shekast korde

--- Euler_py/p9_Euler.py
+++ Euler_py/p9_Euler.py
@@ -1,8 +1,31 @@
-for number in range(0,1000):
-    this_number=str(number).zfill(2)
+def jame_argham(number_digits):
+    sum_digits = 0
+    for k in number_digits:
+        sum_digits += number_digits[k]
+    return sum_digits
 
-    number_digits={}
+def tavan(t):
+    number_digits["a"]**2
+    number_digits["b"]**2
+    number_digits["c"]**2
+
+def number_is_ok(number_digits):
+    if (
+        number_digits["a"]**2 < number_digits["b"]**2
+        and number_digits["b"]**2 < number_digits["c"]**2
+        and number_digits["a"]**2 + number_digits["b"]**2 == number_digits["c"]**2
+    ):
+        if jame_argham(number_digits) == 1000:
+            return True
+
+
+for number in range(0, 1000):
+    this_number = str(number).zfill(3)
+
+    number_digits = {}
     number_digits["a"] = int(this_number[0])
     number_digits["b"] = int(this_number[1])
     number_digits["c"] = int(this_number[2])
-    
+
+    if number_is_ok(number_digits):
+        print(number)
