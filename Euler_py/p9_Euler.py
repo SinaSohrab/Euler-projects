4fc879b6765f7224ea222918330f93b0043aab86
sina <sinasohrab84@gmail.com>
2023-07-21 16:31:31 +0330

    developing

--- Euler_py/p9_Euler.py
+++ Euler_py/p9_Euler.py
@@ -4,13 +4,13 @@ c = 0
 
 
 def bozorg_tar(bg):
-    if a < b and b < c and a**2 + b**2 == c**2:
-        return True
+    for i in range(0,1000):    
+        if a < b and b < c and a**2 + b**2 == c**2:
+            return True
     return False
 
 
 for i in range(0, 1000):
     if bozorg_tar(i):
         for x in range(0,1000):
-            
-
+            print(x)
