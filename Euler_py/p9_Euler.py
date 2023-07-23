dced5d0957be48e9ee963b05b963f6853e55337e
sina <sinasohrab84@gmail.com>
2023-07-23 12:06:59 +0330

    developin

--- Euler_py/p9_Euler.py
+++ Euler_py/p9_Euler.py
@@ -1,15 +1,24 @@
 a = 0
 b = 0
 c = 0
-def abc(i):
-    for i in range(0,1000):
-        for x in range(0,1000):
-            for y in range(0,1000):
-                if a<b and b<c:
-                    return True
-                
-                return False
+number=[]
 
-for i in range(0,1000):
-    if abc(i):
-        print(i)
+for i in range(0, 10):
+    for x in range(0, 10):
+        for y in range(0, 10):
+            a=i
+            b=x
+            c=y
+            if a<b and b<c:
+                i=a**2
+                x=b**2
+                y=c**2
+                number.append(i)
+                number.append(x)
+                number.append(y)
+                if i+x==y:
+                    print(number)
+
+
+print("number ",number)
+
