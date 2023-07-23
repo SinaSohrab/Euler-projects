1b3f6c1962f0e2cb7d60fb13c0cebf67ae8042af
sina <sinasohrab84@gmail.com>
2023-07-23 12:38:00 +0330

    developing

--- Euler_py/p9_Euler.py
+++ Euler_py/p9_Euler.py
@@ -3,22 +3,15 @@ b = 0
 c = 0
 number=[]
 
-for i in range(0, 1000):
-    for x in range(0, 1000):
-        for y in range(0, 1000):
-            a=i
-            b=x
-            c=y
-            if a<b and b<c:
-                ta=a**2
-                tb=b**2
-                tc=c**2
-                jam=ta+tb
-                if jam==tc:
-                    jam_2=ta+tb+tc
-                    if jam_2==1000:
-                        print(jam_2)
+for i in range(0, 10):
+    for x in range(0, 10):
+        for y in range(0, 10):
+            number.append(i)
+            number.append(x)
+            number.append(y)
 
+print(number)
+            
 
 
 
