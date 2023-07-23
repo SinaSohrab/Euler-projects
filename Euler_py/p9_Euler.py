5dab8b950084d6395505a9ab03f542734b0eb41e
sina <sinasohrab84@gmail.com>
2023-07-23 13:45:38 +0330

    developing

--- Euler_py/p9_Euler.py
+++ Euler_py/p9_Euler.py
@@ -6,13 +6,16 @@ number = []
 for i in range(0, 10):
     for x in range(0, 10):
         for y in range(0, 10):
-            number.append(i)
-            number.append(x)
-            number.append(y)
             if i < x and x < y:
+                number.append(i)
+                number.append(x)
+                number.append(y)
                 a = i**2
                 b = x**2
                 c = y**2
-                sum = a + b + c
+                sum = i + x + y
+                sum1=a+b+c
                 if a + b == c:
-                    print(a, b, c)
+                    print(a, b, c, sum1)
+                    if sum1==1000:
+                        print(a, b, c, sum1)
