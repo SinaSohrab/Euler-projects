2ff6f8158fc637a09a16e5b3c830d2d607a3880d
sina <sinasohrab84@gmail.com>
2023-07-23 11:51:43 +0330

    developing

--- Euler_py/p9_Euler.py
+++ Euler_py/p9_Euler.py
@@ -1,11 +1,15 @@
 a = 0
 b = 0
 c = 0
-def abc(i,x,y):
+def abc(i):
     for i in range(0,1000):
         for x in range(0,1000):
             for y in range(0,1000):
                 if a<b and b<c:
                     return True
                 
+                return False
 
+for i in range(0,1000):
+    if abc(i):
+        print(i)
