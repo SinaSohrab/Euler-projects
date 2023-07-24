de034785ccc0e26d1294eb5bbe87a4f45e937c5e
sina <sinasohrab84@gmail.com>
2023-07-24 14:14:38 +0330

    end

--- Euler_py/p16_Euler.py
+++ Euler_py/p16_Euler.py
@@ -1,7 +1,16 @@
-number=2
-t_number=15
+number = 2
+t_number = 1000
 
-j_tavan=number**t_number
+j_tavan = number**t_number
 
 print(j_tavan)
 
+j_tavan_str = str(j_tavan)
+print(type(j_tavan_str))
+sum = 0
+
+for i in j_tavan_str:
+    sum += int(i)
+
+
+print(sum)
