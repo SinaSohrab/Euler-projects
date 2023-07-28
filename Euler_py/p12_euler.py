483cfdb12ad9affebb5bf2fae085ee93ce176cd1
sina <sinasohrab84@gmail.com>
2023-07-28 16:27:44 +0330

    end p12 project

--- Euler_py/p12_euler.py
+++ Euler_py/p12_euler.py
@@ -1,6 +1,23 @@
 number=7
-number+=1
-sum=0
-for i in range(0,number):
-    sum=i+sum
-print(sum)
+def get_divisors_count(n):
+    count = 0
+    for i in range(1, int(n**0.5) + 1):
+        if n % i == 0:
+            if n // i != i:
+                count += 2
+            else:
+                count += 1
+    return count
+
+
+def find_triangle_number_with_divisors_count(divisors_count):
+    n = 1
+    while True:
+        triangle_number = n * (n + 1) // 2
+        divisors_count_of_triangle_number = get_divisors_count(triangle_number)
+        if divisors_count_of_triangle_number >= divisors_count:
+            return triangle_number
+        n += 1
+
+
+print(find_triangle_number_with_divisors_count(number))
