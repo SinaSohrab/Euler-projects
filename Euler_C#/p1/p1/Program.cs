cb3d91d17372b247cd2d6b11ef81232d13c742cf
SinaSohrab <sinasohrab84@gmail.com>
2023-10-19 07:50:53 +0330

    save project

--- Euler_C#/p1/p1/Program.cs
+++ Euler_C#/p1/p1/Program.cs
@@ -13,9 +13,9 @@ namespace p1
             int sum = 0;
             for (int i = 0; i < 1000; i++)
             {
-                if (i%3==0 || i%5==0)
+                if (i % 3 == 0 || i % 5 == 0)
                 {
-                    sum=sum+i;
+                    sum = sum + i;
                 }
             }
             Console.WriteLine(sum);
