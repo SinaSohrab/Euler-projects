926a574e72a6eebf5a77fd5f61062df9c38854ed
SinaSohrab <sinasohrab84@gmail.com>
2023-10-19 07:45:47 +0330

    the end of the p1 c# project

--- Euler_C#/p1/p1/Program.cs
+++ Euler_C#/p1/p1/Program.cs
@@ -10,6 +10,15 @@ namespace p1
     {
         static void Main(string[] args)
         {
+            int sum = 0;
+            for (int i = 0; i < 1000; i++)
+            {
+                if (i%3==0 || i%5==0)
+                {
+                    sum=sum+i;
+                }
+            }
+            Console.WriteLine(sum);
         }
     }
 }
